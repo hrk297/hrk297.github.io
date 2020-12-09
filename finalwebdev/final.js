0f205db2dd78e6d3c6e5fe1dfa8e8d96e9c343b6
Halenur Komsul <hrk297@nyu.edu>
2020-12-09 13:15:58 -0500

    final

---
+++ finalwebdev/final.js
@@ -0,0 +1,38 @@
+// 	var images = new Array();
+// 	images[0] = "loas.jpg";
+// 	images[1] = "siverek3.png";
+// 	images[2] = "russiaithink.png";
+// 	images[3] = "pretoria.jpg";
+// 	images[4] = "siverek2.png";
+
+
+// var i=0;
+// //function for dynamically switching the background image
+// function changeBg(divId) {
+//  if (i==5){
+//   i=0;
+//  }
+//  document.getElementById('footer').style.backgroundImage = 'url('+images[i]+')';
+//  i++;
+
+// // }
+// // /*]]>*/
+
+//     // $("#myLink").click(function() { 
+//     //            // ^ Attach a function on click event of an element with id="mylink"
+//     //   //Change the css of a element with id="myDiv"
+//     //   $("#myDiv").html("backgroundImage", "url(http://www.google.com/logos/2012/spring12-hp.jpg)");
+//     //                // Note ^: In jQuery, the parameters with "-" as define that way
+//     //                // But of course .css() supports "-" also, but make a habit of doing in camel-case
+//     // });
+
+// document.getElementById("demo").onclick = function() {myFunction()};
+
+// // function myFunction() {
+// //   document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+// // }
+
+// function changeAsset(newAsset) {
+//   var elem = document.getElementById('demo');
+//   elem.style.Asset = newAsset;
+// }
