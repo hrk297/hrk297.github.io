a648d57056eee38ea1c6ece28dfb82608efa5d9b
Halenur Komsul <hrk297@nyu.edu>
2020-12-12 22:45:34 -0500

    d

---
+++ finalfinal/final.js
@@ -0,0 +1,103 @@
+// 	var images = new Array();
+// 	page1 = "loas.jpg";
+// 	page2 = "siverek3.png";
+// 	page3 = "russiaithink.png";
+// 	page4 = "pretoria.jpg";
+// 	page 5 = "siverek2.png";
+
+// var i=0;
+// //function for dynamically switching the background image
+// function changeBg(divId) {
+//  if (i==5){
+//   i=0;
+//  }
+//  document.getElementById('footer').style.backgroundImage = 'url('+images[i]+')';
+//  i++;
+
+// // }
+// // /*]]>*/
+
+//     // $("#myLink").click(function() { 
+//     //            // ^ Attach a function on click event of an element with id="mylink"
+//     //   //Change the css of a element with id="myDiv"
+//     //   $("#myDiv").html("backgroundImage", "url(http://www.google.com/logos/2012/spring12-hp.jpg)");
+//     //                // Note ^: In jQuery, the parameters with "-" as define that way
+//     //                // But of course .css() supports "-" also, but make a habit of doing in camel-case
+//     // });
+
+// document.getElementById("demo").onclick = function() {myFunction()};
+
+// // function myFunction() {
+// //   document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+// // }
+
+// function changeAsset(newAsset) {
+//   var elem = document.getElementById('demo');
+//   elem.style.Asset = newAsset;
+// }
+
+// $(document).ready(function(){
+// 	$('.showbtn').click(function(){
+//    		$('.myimgdivshowhide').show();
+// 	});
+
+function getConfirmation1() {
+               if(confirm("are you ready?" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation2() {
+               if(confirm("don't forget that karma is a full circle" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation3() {
+               if(confirm("like a serpent eating its own body." ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation4() {
+               if(confirm("godspeed to you" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation5() {
+               if(confirm("and godspeed to us" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation6() {
+               if(confirm("please never stop trying" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
---
+++ finalwebdev/finaljs.js
@@ -0,0 +1,60 @@
+function getConfirmation1() {
+               if(confirm("are you ready?" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation2() {
+               if(confirm("don't forget that karma is a full circle" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation3() {
+               if(confirm("like a serpent eating its own body." ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation4() {
+               if(confirm("godspeed to you" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation5() {
+               if(confirm("and godspeed to us" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
+function getConfirmation6() {
+               if(confirm("please never stop trying" ) == true ) {
+
+                return true;
+               } 
+               else {
+                    console.log('The User Decided To Terminate This Experience');
+                return false;
+               }
+            }
